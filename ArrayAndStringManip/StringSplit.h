144187331ed4d13747ff539ee755dd10562fe463
Brian Szuter <BrianSzuter@users.noreply.github.com>
2016-09-16 20:14:12 -0400

    Added description to StringSplit.

--- ArrayAndStringManip/StringSplit.h
+++ ArrayAndStringManip/StringSplit.h
@@ -6,5 +6,8 @@
 
 namespace ArrayAndStringManip
 {
+	// Split a string into a vector of strings based on a delimiter character.
+	// Multiple adjacent delimiters together are treated as a single delimiter.
+	// An empty string is never added to the result vector.
 	std::vector<std::string> STRING_MANIP_DLLEXPORT StringSplit(const std::string& str, char delim);
 }
